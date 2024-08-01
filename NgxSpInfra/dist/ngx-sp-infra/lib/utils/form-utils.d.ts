82f4ea79017763f265197443833e3ec906b38022
ERICK Carvalho Paulette de Oliveira <erickzcarvalho25@outlook.com>
2024-08-01 15:53:06 -0300

    correção menor

--- NgxSpInfra/dist/ngx-sp-infra/lib/utils/form-utils.d.ts
+++ NgxSpInfra/dist/ngx-sp-infra/lib/utils/form-utils.d.ts
@@ -1,7 +1,11 @@
 import { AbstractControl, NgControl, UntypedFormGroup, UntypedFormArray } from '@angular/forms';
 export declare class FormUtils {
+    /** Verifica se um campo é inválido (Template Driven) */
     static isInvalidFieldTemplate(control: NgControl): boolean;
+    /** Verifica se um campo é inválido (Data Driven) */
     static isInvalidField(control: AbstractControl | null): boolean;
+    /** Valida todos os campos do formulário */
     static validateFields(formGroup: UntypedFormGroup | UntypedFormArray): void;
+    /** Obtém a mensagem de erro conforme o validador utilizado */
     static getErrorMessage(fieldName: string, validatorName: string, validatorValue?: any): string;
 }
