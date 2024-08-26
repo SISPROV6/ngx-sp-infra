5c539b567150c91910782384a0e84f52d70bcb92
ERICK Carvalho Paulette de Oliveira <erickzcarvalho25@outlook.com>
2024-08-26 14:19:21 -0300

    v1.9.3 | Commit automático

--- projects/ngx-sp-infra/src/scripts/commit.js
+++ projects/ngx-sp-infra/src/scripts/commit.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const execSync = require('child_process').execSync;
 
 // Função para atualizar a versão
-function commit() {
+function commit(branch) {
   // Lê o package.json
   const packageJson = JSON.parse(fs.readFileSync('projects/ngx-sp-infra/package.json', 'utf8'));
 
@@ -15,7 +15,9 @@ function commit() {
   // Adiciona alterações no commit e realiza o push
   execSync(`git add .`, { stdio: 'inherit' });
   execSync(`git commit --allow-empty -m "v${packageVersion} | Commit automático"`, { stdio: 'inherit' });
-  execSync(`git push origin main`, { stdio: 'inherit' });
+  execSync(`git push origin ${branch}`, { stdio: 'inherit' });
 }
 
-commit();
+// Branch que será selecionada
+const branch = process.argv[2];
+commit(branch);
